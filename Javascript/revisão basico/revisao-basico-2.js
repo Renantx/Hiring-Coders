88c2c61ba1a4f872021bc41703b06734e485d5de
Renantx <66394069+Renantx@users.noreply.github.com>
2021-07-09 16:13:30 -0300

    Update revisao-basico-2.js

--- Javascript/revisão basico/revisao-basico-2.js
+++ Javascript/revisão basico/revisao-basico-2.js
@@ -1,2 +1,20 @@
 //Crie um algoritimo que receba três notas de um aluno, calcule a média e mostre as seguintes mensagens de acordo com cada situação:
-//
+// - Se a média for maior ou igual a 7 - Aprovado
+// - Se a média for maior ou igual a 5 - Recuperação
+// - Se a média for menor que 5 - Reprovado
+
+function calculaMedia(nota1, nota2, nota3) {
+    let media = (nota1, nota2, nota3) / 3;
+
+    if (media >= 7) {
+        return 'Aprovado';
+    }
+
+    if (media >= 5 && media < 7) {
+        return 'Recuperação';
+    }
+
+    if (media < 5) {
+        return 'Reprovado';
+    }
+}
